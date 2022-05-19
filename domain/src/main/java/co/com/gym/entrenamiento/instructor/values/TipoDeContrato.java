28b49fba65d3ad02aa4f730e72683590153b050e
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-19 18:42:24 -0500

    Values - Instructor
    Se agrega los values de instructor

---
+++ domain/src/main/java/co/com/gym/entrenamiento/instructor/values/TipoDeContrato.java
@@ -0,0 +1,26 @@
+package co.com.gym.entrenamiento.instructor.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class TipoDeContrato implements ValueObject<String> {
+    private final String tipoDeContrato;
+    private final String detalles;
+
+    public TipoDeContrato(String tipoDeContrato, String detalles) {
+        this.tipoDeContrato = Objects.requireNonNull(tipoDeContrato);
+        if(this.tipoDeContrato.isBlank()){
+            throw new IllegalArgumentException("el tipo de contrato no puede estar en blanco");
+        }
+        this.detalles = Objects.requireNonNull(detalles);
+        if(this.detalles.isBlank()){
+            throw  new IllegalArgumentException("Los detalles no pueden estar vacios");
+        }
+    }
+
+    @Override
+    public String value() {
+        return tipoDeContrato;
+    }
+}
